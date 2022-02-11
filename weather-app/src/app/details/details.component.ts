a0841da552a372ae234ef5dcf8efda4c5de36d83
Olekciiy Anurov-Prihodko <anyrov.prihodko@gmail.com>
2022-02-11 12:00:11 +0200

    [WA-6]: add navigation

--- weather-app/src/app/details/details.component.ts
+++ weather-app/src/app/details/details.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  openForecastPage() {
+    this.router.navigate(['/:city/details/forecast']);
   }
-
 }
--- weather-app/src/app/forecast/forecast.component.ts
+++ weather-app/src/app/forecast/forecast.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forecast',
   templateUrl: './forecast.component.html',
-  styleUrls: ['./forecast.component.scss']
+  styleUrls: ['./forecast.component.scss'],
 })
 export class ForecastComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  openDetailsPage() {
+    this.router.navigate(['/:city/details']);
   }
 
+  openHomePage() {
+    this.router.navigate(['/']);
+  }
 }
