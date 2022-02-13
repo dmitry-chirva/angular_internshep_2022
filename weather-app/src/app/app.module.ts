9df56289fa87d79c2853128df422253990352f31
uselesswhale1 <daniashevchenko@gmail.com>
2022-02-13 21:13:47 +0200

    added swither component in header, added styles and markup

--- weather-app/src/app/app.module.ts
+++ weather-app/src/app/app.module.ts
@@ -5,9 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { ThemeSwitcherComponent } from './header/theme-switcher/theme-switcher.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ThemeSwitcherComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
