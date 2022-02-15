2f6e2b312d1494eb086e702445cf29439b14bb47
Chakipaki <belyaeve2@icloud.com>
2022-02-15 12:34:04 +0200

    [WA-14]: Forecast list: create bread crumbs, City, buttons and slyled their

--- weather-app/src/app/forecast/forecast.component.ts
+++ weather-app/src/app/forecast/forecast.component.ts
@@ -9,6 +9,8 @@ import { Router } from '@angular/router';
 export class ForecastComponent implements OnInit {
   constructor(private router: Router) {}
 
+  public city: string = "Kyiv, Ukraine";
+
   ngOnInit(): void {}
 
   openDetailsPage() {
