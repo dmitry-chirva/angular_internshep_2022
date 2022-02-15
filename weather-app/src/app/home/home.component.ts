0a37c020ea14c9a849c2fa4a6a7da4bfc5acab59
Kateryna Kraieva <70651413+ketryin@users.noreply.github.com>
2022-02-15 18:29:07 +0200

    [WA-5]: addes breadcrumbs on pages

--- weather-app/src/app/home/home.component.ts
+++ weather-app/src/app/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +6,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor() { }
 
-  ngOnInit(): void {}
-  openDetailsPage() {
-    this.router.navigate(['/:city/details']);
+  ngOnInit(): void {
   }
 }
