42f5ca2a1b42a9700e17f7a95ee1b88f2feca444
Kateryna Kraieva <70651413+ketryin@users.noreply.github.com>
2022-02-15 18:34:04 +0200

    [WA-7]: adds routes for links

--- weather-app/src/app/shared/components/current-weather/current-weather.component.ts
+++ weather-app/src/app/shared/components/current-weather/current-weather.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-current-weather',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurrentWeatherComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+    openDetailsPage() {
+    this.router.navigate(['/:city/details']);
   }
 
 }
