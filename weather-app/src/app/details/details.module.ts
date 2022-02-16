ddf8398be1b72ddc252885fc85f2d65753603fcd
Sasha <olstoliar99@gmail.com>
2022-02-16 10:22:42 +0200

    [WA-8]: details.module.ts was modified

--- weather-app/src/app/details/details.module.ts
+++ weather-app/src/app/details/details.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { DetailsTableComponent } from './details-table/details-table.component';
+import { DetailsBtnsComponent } from './details-btns/details-btns.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [DetailsTableComponent, DetailsBtnsComponent],
+  imports: [CommonModule],
+  exports: [DetailsTableComponent, DetailsBtnsComponent]
 })
 export class DetailsModule { }
