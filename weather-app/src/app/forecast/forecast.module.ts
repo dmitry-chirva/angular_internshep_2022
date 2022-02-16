59c0b688613a31a7da257cf6fb80560db9853064
Kateryna Kraieva <70651413+ketryin@users.noreply.github.com>
2022-02-16 13:21:21 +0200

    [WA-5]: addes BreadcrumbsComponent to forecast and details

--- weather-app/src/app/forecast/forecast.module.ts
+++ weather-app/src/app/forecast/forecast.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
+import { BreadcrumbsComponent } from '../shared/components/breadcrumbs/breadcrumbs.component';
 
 
 
@@ -9,6 +10,7 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     CommonModule,
     SharedModule
-  ]
+  ],
+  exports: [BreadcrumbsComponent]
 })
 export class ForecastModule { }
