3a79902ef805584dff757b721b48080dd66625da
Kateryna Kraieva <70651413+ketryin@users.noreply.github.com>
2022-02-21 19:23:41 +0200

    [WA-22]: adds mock data

--- src/app/details/components/details-table/details-table.component.ts
+++ src/app/details/components/details-table/details-table.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { DetailsInfo } from 'src/app/shared/interfaces/details-info.interfaces';
 
 @Component({
   selector: 'app-details-table',
   templateUrl: './details-table.component.html',
   styleUrls: ['./details-table.component.scss']
 })
-export class DetailsTableComponent implements OnInit {
+export class DetailsTableComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+ details: DetailsInfo[] = [
+    {
+      hour: '12AM',
+      temperature: -1,
+      feelslike_c: -5,
+      wind_kph: 4,
+      gust_kph: 5,
+      cloud: 80,
+      humidity: 92,
+      pressure_mb: 735  
+    },
+    {
+      hour: '6AM',
+      temperature: -1,
+      feelslike_c: -5,
+      wind_kph: 4,
+      gust_kph: 5,
+      cloud: 80,
+      humidity: 92,
+      pressure_mb: 735  
+    },
+    {
+      hour: '12AM',
+      temperature: -1,
+      feelslike_c: -5,
+      wind_kph: 4,
+      gust_kph: 5,
+      cloud: 80,
+      humidity: 92,
+      pressure_mb: 735  
+    },
+    {
+      hour: '12AM',
+      temperature: -1,
+      feelslike_c: -5,
+      wind_kph: 4,
+      gust_kph: 5,
+      cloud: 80,
+      humidity: 92,
+      pressure_mb: 735  
+    },
+  ]
 }
