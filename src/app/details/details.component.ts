209994e392b0e7e082b3791864142cc3d8239c03
Olekciiy Anurov-Prihodko <anyrov.prihodko@gmail.com>
2022-03-09 15:59:16 +0200

    [WA-31]: refactoring, added catchError

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -51,48 +51,13 @@ export class DetailsComponent {
           humidity,
           pressure_mb,
         }: DataDetailsWeather) => {
-          this.detailsWeatherData.temp_c = [
-            Math.round(temp_c[0]),
-            Math.round(temp_c[6]),
-            Math.round(temp_c[12]),
-            Math.round(temp_c[18]),
-          ];
-          this.detailsWeatherData.feelslike_c = [
-            Math.round(feelslike_c[0]),
-            Math.round(feelslike_c[6]),
-            Math.round(feelslike_c[12]),
-            Math.round(feelslike_c[18]),
-          ];
-          this.detailsWeatherData.wind_kph = [
-            +(wind_kph[0] * 0.277).toFixed(1),
-            +(wind_kph[6] * 0.277).toFixed(1),
-            +(wind_kph[12] * 0.277).toFixed(1),
-            +(wind_kph[18] * 0.277).toFixed(1),
-          ];
-          this.detailsWeatherData.gust_kph = [
-            +(gust_kph[0] * 0.277).toFixed(1),
-            +(gust_kph[6] * 0.277).toFixed(1),
-            +(gust_kph[12] * 0.277).toFixed(1),
-            +(gust_kph[18] * 0.277).toFixed(1),
-          ];
-          this.detailsWeatherData.cloud = [
-            cloud[0],
-            cloud[6],
-            cloud[12],
-            cloud[18],
-          ];
-          this.detailsWeatherData.humidity = [
-            humidity[0],
-            humidity[6],
-            humidity[12],
-            humidity[18],
-          ];
-          this.detailsWeatherData.pressure_mb = [
-            pressure_mb[0],
-            pressure_mb[6],
-            pressure_mb[12],
-            pressure_mb[18],
-          ];
+          this.detailsWeatherData.temp_c = [...temp_c];
+          this.detailsWeatherData.feelslike_c = [...feelslike_c];
+          this.detailsWeatherData.wind_kph = [...wind_kph];
+          this.detailsWeatherData.gust_kph = [...gust_kph];
+          this.detailsWeatherData.cloud = [...cloud];
+          this.detailsWeatherData.humidity = [...humidity];
+          this.detailsWeatherData.pressure_mb = [...pressure_mb];
         }
       );
   }
--- src/core/api/details/details.service.ts
+++ src/core/api/details/details.service.ts
@@ -16,47 +16,150 @@ export class DetailsService {
   ) {}
 
   getDataForWeatherTable(): Observable<DataDetailsWeather | any> {
-    return this.geolocationService.getPosition().pipe(
-      switchMap((pos) => {
-        const latitude = pos.coords.latitude;
-        const longitude = pos.coords.longitude;
-        return this.weatherService.getCurrentWeather(latitude, longitude).pipe(
-          switchMap((cw): any => {
-            const cityName = cw.location.name;
-            return this.weatherService.getForecastWeather(cityName).pipe(
-              map((data) => {
-                const temp_c: Array<number> = [];
-                const feelslike_c: Array<number> = [];
-                const wind_kph: Array<number> = [];
-                const gust_kph: Array<number> = [];
-                const cloud: Array<number> = [];
-                const humidity: Array<number> = [];
-                const pressure_mb: Array<number> = [];
+    return this.geolocationService
+      .getPosition()
+      .pipe(
+        switchMap((pos) => {
+          const latitude = pos.coords.latitude;
+          const longitude = pos.coords.longitude;
+          return this.weatherService
+            .getCurrentWeather(latitude, longitude)
+            .pipe(
+              switchMap((cw): any => {
+                const cityName = cw.location.name;
+                return this.weatherService.getForecastWeather(cityName).pipe(
+                  map((data) => {
+                    const temp_c: Array<number> = [];
+                    const feelslike_c: Array<number> = [];
+                    const wind_kph: Array<number> = [];
+                    const gust_kph: Array<number> = [];
+                    const cloud: Array<number> = [];
+                    const humidity: Array<number> = [];
+                    const pressure_mb: Array<number> = [];
 
-                data.forecast.forecastday[0].hour.map((v) => {
-                  temp_c.push(v.temp_c);
-                  feelslike_c.push(v.feelslike_c);
-                  wind_kph.push(v.wind_kph);
-                  gust_kph.push(v.gust_kph);
-                  cloud.push(v.cloud);
-                  humidity.push(v.humidity);
-                  pressure_mb.push(v.pressure_mb);
-                });
-                return {
-                  temp_c,
-                  feelslike_c,
-                  wind_kph,
-                  gust_kph,
-                  cloud,
-                  humidity,
-                  pressure_mb,
-                };
+                    data.forecast.forecastday[0].hour.map((v) => {
+                      temp_c.push(v.temp_c);
+                      feelslike_c.push(v.feelslike_c);
+                      wind_kph.push(v.wind_kph);
+                      gust_kph.push(v.gust_kph);
+                      cloud.push(v.cloud);
+                      humidity.push(v.humidity);
+                      pressure_mb.push(v.pressure_mb);
+                    });
+                    return {
+                      temp_c: [
+                        Math.round(temp_c[0]),
+                        Math.round(temp_c[6]),
+                        Math.round(temp_c[12]),
+                        Math.round(temp_c[18]),
+                      ],
+                      feelslike_c: [
+                        Math.round(feelslike_c[0]),
+                        Math.round(feelslike_c[6]),
+                        Math.round(feelslike_c[12]),
+                        Math.round(feelslike_c[18]),
+                      ],
+                      wind_kph: [
+                        +(wind_kph[0] * 0.277).toFixed(1),
+                        +(wind_kph[6] * 0.277).toFixed(1),
+                        +(wind_kph[12] * 0.277).toFixed(1),
+                        +(wind_kph[18] * 0.277).toFixed(1),
+                      ],
+                      gust_kph: [
+                        +(gust_kph[0] * 0.277).toFixed(1),
+                        +(gust_kph[6] * 0.277).toFixed(1),
+                        +(gust_kph[12] * 0.277).toFixed(1),
+                        +(gust_kph[18] * 0.277).toFixed(1),
+                      ],
+                      cloud: [cloud[0], cloud[6], cloud[12], cloud[18]],
+                      humidity: [
+                        humidity[0],
+                        humidity[6],
+                        humidity[12],
+                        humidity[18],
+                      ],
+                      pressure_mb: [
+                        pressure_mb[0],
+                        pressure_mb[6],
+                        pressure_mb[12],
+                        pressure_mb[18],
+                      ],
+                    };
+                  })
+                );
               })
             );
-          })
-        );
-      })
-    );
+        })
+      )
+      .pipe(
+        catchError(() =>
+          this.weatherService.getForecastWeather().pipe(
+            switchMap((cw): any => {
+              const cityName = cw.location.name;
+              return this.weatherService.getForecastWeather(cityName).pipe(
+                map((data) => {
+                  const temp_c: Array<number> = [];
+                  const feelslike_c: Array<number> = [];
+                  const wind_kph: Array<number> = [];
+                  const gust_kph: Array<number> = [];
+                  const cloud: Array<number> = [];
+                  const humidity: Array<number> = [];
+                  const pressure_mb: Array<number> = [];
+
+                  data.forecast.forecastday[0].hour.map((v) => {
+                    temp_c.push(v.temp_c);
+                    feelslike_c.push(v.feelslike_c);
+                    wind_kph.push(v.wind_kph);
+                    gust_kph.push(v.gust_kph);
+                    cloud.push(v.cloud);
+                    humidity.push(v.humidity);
+                    pressure_mb.push(v.pressure_mb);
+                  });
+                  return {
+                    temp_c: [
+                      Math.round(temp_c[0]),
+                      Math.round(temp_c[6]),
+                      Math.round(temp_c[12]),
+                      Math.round(temp_c[18]),
+                    ],
+                    feelslike_c: [
+                      Math.round(feelslike_c[0]),
+                      Math.round(feelslike_c[6]),
+                      Math.round(feelslike_c[12]),
+                      Math.round(feelslike_c[18]),
+                    ],
+                    wind_kph: [
+                      +(wind_kph[0] * 0.277).toFixed(1),
+                      +(wind_kph[6] * 0.277).toFixed(1),
+                      +(wind_kph[12] * 0.277).toFixed(1),
+                      +(wind_kph[18] * 0.277).toFixed(1),
+                    ],
+                    gust_kph: [
+                      +(gust_kph[0] * 0.277).toFixed(1),
+                      +(gust_kph[6] * 0.277).toFixed(1),
+                      +(gust_kph[12] * 0.277).toFixed(1),
+                      +(gust_kph[18] * 0.277).toFixed(1),
+                    ],
+                    cloud: [cloud[0], cloud[6], cloud[12], cloud[18]],
+                    humidity: [
+                      humidity[0],
+                      humidity[6],
+                      humidity[12],
+                      humidity[18],
+                    ],
+                    pressure_mb: [
+                      pressure_mb[0],
+                      pressure_mb[6],
+                      pressure_mb[12],
+                      pressure_mb[18],
+                    ],
+                  };
+                })
+              );
+            })
+          )
+        )
+      );
   }
 
   getCurrentWeatherDetails(): Observable<CurrentWeatherData> {
