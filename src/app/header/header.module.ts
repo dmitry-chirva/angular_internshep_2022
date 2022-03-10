dcf5fc70d1f89c7b6e9fc673b2fd6deb86bfce7b
Chakipaki <belyaeve2@icloud.com>
2022-03-10 13:14:08 +0200

    [WA-26]: Autocomplete init

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { SharedModule } from '../shared/shared.module';
 import { HeaderComponent } from './header.component';
+import {SearchComponent} from "./search/search.component";
 
 @NgModule({
-  declarations: [HeaderComponent],
-  imports: [SharedModule],
+  declarations: [HeaderComponent, SearchComponent],
+  imports: [SharedModule, ReactiveFormsModule],
   exports: [HeaderComponent],
 })
 export class HeaderModule {}
---
+++ src/app/header/search/search.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss']
+})
+export class SearchComponent implements OnInit {
+
+  search = new FormControl('')
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
