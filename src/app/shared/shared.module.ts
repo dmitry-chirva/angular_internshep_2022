452b7e64420c8fc8ef49473fd9d4a524d03919e6
Daniil Shevchenko <54992950+uselesswhale1@users.noreply.github.com>
2022-03-11 14:09:52 +0200

    Update shared.module.ts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+
 import { CurrentWeatherComponent } from './components/current-weather/current-weather.component';
 import { ThemeSwitcherComponent } from './components/theme-switcher/theme-switcher.component';
 import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
@@ -9,8 +11,6 @@ import { BtnGroupsComponent } from './components/btn-groups/btn-groups.component
 import { FavoriteToggleComponent } from './components/favorite-toggle/favorite-toggle.component';
 import { NotificationComponent } from './components/notification/notification.component';
 import { ForecastCardComponent } from './components/forecast-card/forecast-card.component';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
