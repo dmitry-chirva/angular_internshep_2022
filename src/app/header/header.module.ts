488718c394659405b3a46bef495554ae3c65e2ee
Chakipaki <belyaeve2@icloud.com>
2022-03-11 12:55:58 +0200

    [WA-26]: Autocomplete: add hardcode state to component.ts create function to search cities

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import {CommonModule} from "@angular/common";
 
 import { SharedModule } from '../shared/shared.module';
 import { HeaderComponent } from './header.component';
@@ -7,7 +8,7 @@ import {SearchComponent} from "./search/search.component";
 
 @NgModule({
   declarations: [HeaderComponent, SearchComponent],
-  imports: [SharedModule, ReactiveFormsModule],
+  imports: [SharedModule, ReactiveFormsModule, CommonModule],
   exports: [HeaderComponent],
 })
 export class HeaderModule {}
--- src/app/header/search/search.component.ts
+++ src/app/header/search/search.component.ts
@@ -9,6 +9,23 @@ import { FormControl } from '@angular/forms';
 export class SearchComponent implements OnInit {
 
   search = new FormControl('')
+  searchResult: string[] = []
+  cityState = ['Oslo', 'New York', 'Manchester', 'London', 'York', 'Stockholm', 'Kiev', 'Poznan', 'Amsterdam']
+  notDefined = ''
+
+  searching(event :any) {
+    if(this.search.value != 0) {
+      this.searchResult = this.cityState.filter((e) => e.slice(0, this.search.value.length).toLowerCase() == this.search.value.toLowerCase() ? e : null)
+      if (this.searchResult.length === 0) {
+        this.notDefined = 'Not defined'
+      } else {
+        this.notDefined = ''
+      }
+    } else {
+      this.searchResult = []
+      this.notDefined = ''
+    }
+  }
 
   constructor() { }
 
