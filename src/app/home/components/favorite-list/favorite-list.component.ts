cf4e92b00a7545d3f3d6cd384a1cf2739df52285
uselesswhale1 <daniashevchenko@gmail.com>
2022-03-18 10:56:49 +0200

    added ngOnInit to fav-list comp

--- src/app/home/components/favorite-list/favorite-list.component.ts
+++ src/app/home/components/favorite-list/favorite-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { CityWeatherInfo } from 'src/app/shared/interfaces/city-weather-info.interfaces';
 import { FavoriteStateService } from 'src/core/favorites-state/favorite-state.service';
@@ -10,21 +10,14 @@ import { FavoriteService } from 'src/core/favorite/favorite.service';
   styleUrls: ['./favorite-list.component.scss'],
   providers: [FavoriteStateService, StorageService],
 })
-export class FavoriteListComponent  {
+export class FavoriteListComponent implements OnInit  {
   favorites: CityWeatherInfo[] = [];
 
   constructor(private favoriteService: FavoriteService) {
-
-    // this.favorites = this.favoriteService.favorites;
-    this.favorites = this.favoriteService.getFavorites();
-
-    // if(this.favoriteService.checkAmountOfFavorites()){
-    //   // this.notificationService.show('Advise', `There is less than ${this.favoriteService.maxAmountOfFavs} favorite cities in your list, you can add more!`);
-    // }
-    // else{
-    //   // this.notificationService.show('Error', `There is max amount of cities in your list of favorites!`);
-    // };
   }
 
+  ngOnInit(): void {
+    this.favorites = this.favoriteService.favorites;
+  }
 
 }
