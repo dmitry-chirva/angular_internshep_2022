640b9520c9f720af8452504cadbdb5799c1c0bbd
Chakipaki <belyaeve2@icloud.com>
2022-03-21 17:40:55 +0200

    [WA-27]: init service

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,7 @@
 import { Component } from '@angular/core';
-import {SearchService} from "../../core/api/search/search.service";
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
-
-  constructor(private search: SearchService) {
-    this.search.getSearchList('lon')
-  }
-}
+export class HeaderComponent {}
