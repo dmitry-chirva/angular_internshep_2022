044e0d2b3c409163684e93c38b76e865f1cc9742
Sasha <olstoliar99@gmail.com>
2022-03-21 19:47:30 +0200

    [WA-34]: dark mode was created

--- src/app/shared/components/theme-switcher/theme-switcher.component.ts
+++ src/app/shared/components/theme-switcher/theme-switcher.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-theme-switcher',
@@ -6,14 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./theme-switcher.component.scss'],
 })
 export class ThemeSwitcherComponent implements OnInit {
-  constructor() {}
+  themes: Themes = 'light-theme';
+
+  constructor(@Inject(DOCUMENT) private document: Document,
+              private render: Renderer2) {}
+
   lightTheme = 'light';
   darkTheme = 'dark';
   theme = this.lightTheme;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initializeTheme();
+  }
+
+  switchTheme() {
+    this.document.body.classList.replace(
+      this.themes, 
+      this.themes === 'light-theme' 
+        ? (this.themes = 'dark-theme') 
+        : (this.themes = 'light-theme')
+      );
+  }
+
+  initializeTheme = (): void => 
+    this.render.addClass(this.document.body, this.themes)
 
   onToggle(theme: string): void {
     this.theme = theme === this.lightTheme ? this.darkTheme : this.lightTheme;
   }
 }
+
+export type Themes = 'light-theme' | 'dark-theme';
