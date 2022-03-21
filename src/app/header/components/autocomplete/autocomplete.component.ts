fdff043f9ee7aefa5276cb34a99073d974dda654
Chakipaki <belyaeve2@icloud.com>
2022-03-21 18:54:41 +0200

    [WA-26]: Autocomplete: delete methods in header components

--- src/app/header/components/autocomplete/autocomplete.component.ts
+++ src/app/header/components/autocomplete/autocomplete.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {CitySearchInterface} from "../../../shared/interfaces/city-search.interface";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-autocomplete',
@@ -9,8 +10,11 @@ import {CitySearchInterface} from "../../../shared/interfaces/city-search.interf
 
 export class AutocompleteComponent {
 
+  constructor(private http: HttpClient) {
+  }
+
   keyword = 'name';
-  data = [
+  data: any = [
     {
       id: 1,
       name: 'Georgia'
@@ -35,7 +39,6 @@ export class AutocompleteComponent {
   }
 
   onChangeSearch(val: string) {
-
   }
 
   onFocused(e: any) {
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,23 +1,8 @@
 import {Component} from '@angular/core';
-import {FormControl} from "@angular/forms";
-import {HttpClient} from "@angular/common/http";
-import {CitySearchInterface} from "../shared/interfaces/city-search.interface";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
-  search = new FormControl('');
-
-  citiesList: CitySearchInterface[] = []
-
-  constructor(private http: HttpClient) { }
-
-  getCities (): any {
-    this.http.get<[]>(
-      `http://api.weatherapi.com/v1/search.json?key=b88614b3fb684e8b996104153220302&q=${this.search.value}`
-    ).subscribe((data) => this.citiesList = data)
-  }
-}
+export class HeaderComponent {}
