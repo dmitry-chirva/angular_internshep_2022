f80d33e2fa090ce241ff590ad68315f357cdfec2
uselesswhale1 <daniashevchenko@gmail.com>
2022-03-23 10:45:32 +0200

    [WA-30] Implemented local storage for fav-list, created logic for specific weather

--- src/app/shared/components/favorite-toggle/favorite-toggle.component.ts
+++ src/app/shared/components/favorite-toggle/favorite-toggle.component.ts
@@ -1,21 +1,24 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { NotificationService } from 'src/core/api/notification/notification.service';
 import { NotificationType } from '../../enums/notification.enum';
-
+import { FavoriteStateService } from './../../../../core/favorites-state/favorite-state.service';
 @Component({
   selector: 'app-favorite-toggle',
   templateUrl: 'favorite-toggle.component.html',
   styleUrls: [],
 })
 export class FavoriteToggleComponent {
-  constructor(private notificationService: NotificationService) {}
+  constructor(private notificationService: NotificationService, private favoriteStateService: FavoriteStateService) {}
   @Input() isFavorite: boolean | undefined;
   @Output() onChange = new EventEmitter<boolean | undefined>();
 
   onToggle() {
     this.isFavorite = !this.isFavorite;
     this.onChange.emit(this.isFavorite);
-
+    this.favoriteStateService.addFavoriteCity('Toronto');
+    this.favoriteStateService.addFavoriteCity('Kharkiv');
+    this.favoriteStateService.addFavoriteCity('Paris');
+    this.favoriteStateService.addFavoriteCity('Barcelona');
     this.notificationService.show(NotificationType.Info,'You clicked on heart icon');
   }
 }
--- src/core/favorite/favorite.service.ts
+++ src/core/favorite/favorite.service.ts
@@ -1,85 +1,42 @@
-import { CityWeatherInfo } from 'src/app/shared/interfaces/city-weather-info.interfaces';
 import { Injectable } from '@angular/core';
+import { CityWeatherInfo } from 'src/app/shared/interfaces/city-weather-info.interfaces';
 import { FavoriteStateService } from 'src/core/favorites-state/favorite-state.service';
-import { Observable, map } from 'rxjs';
-import { WeatherService } from '../api/weather/weather.service';
 import { WeatherTransformService } from './../api/common/weather-transform.service';
+import { WeatherService } from '../api/weather/weather.service';
+import { map } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class FavoriteService {
 
-  maxAmountOfFavs: number = 10;
   favorites: CityWeatherInfo[] = [];
-  testFavorites: CityWeatherInfo[] = [
-    {
-      city: 'Toronto',
-      date: '2022-02-18',
-      temp: '1 °С',
-      isFavorite: true,
-      weatherIcon: 'assets/images/weather.png',
-      additionalInfo: {
-        weatherLabel: 'Light snow',
-        windSpeed: '3 km/h',
-        humidity: '72%',
-      },
-    },
-    {
-      city: 'Stockholm',
-      date: '2022-02-18',
-      temp: '-3 °С',
-      isFavorite: true,
-      weatherIcon: 'assets/images/weather.png',
-      additionalInfo: {
-        weatherLabel: 'Light snow',
-        windSpeed: '5 km/h',
-        humidity: '85%',
-      },
-    },
-    {
-      city: 'Oslo',
-      date: '2022-02-18',
-      temp: '-5 °С',
-      isFavorite: true,
-      weatherIcon: 'assets/images/weather.png',
-      additionalInfo: {
-        weatherLabel: 'Light snow',
-        windSpeed: '10 km/h',
-        humidity: '90%',
-      },
-    }
-  ];
-  favoritesNames: string[] = ['Toronto','Oslo','Odessa'];
+  favoritesNames: string[] = [];
 
-  constructor(public favoriteStateService: FavoriteStateService, private weatherService: WeatherService, private weatherTransformService: WeatherTransformService) {
+  constructor(
+    private favoriteStateService: FavoriteStateService,
+    private weatherService: WeatherService,
+    private weatherTransformService: WeatherTransformService
+  ) {
+    this.favoritesNames = this.favoriteStateService.getFavoriteCities();
+  }
+  ngOnInit(){
+    this.favoritesNames = this.favoriteStateService.getFavoriteCities();
   }
 
   getFavorites(): CityWeatherInfo[] {
-    let result: CityWeatherInfo[] = [];
-
     this.favoritesNames.map( city => {
-
-      this.getCurrentWeatherForecast(city, 1)
-      .subscribe((day) => {
-        this.favorites.push(day);
-        console.log(this.favorites);
-      });
-    })
-    return result;
-  }
-
-  getCurrentWeatherForecast(
-    city: string,
-    days: number
-  ): Observable<CityWeatherInfo> {
-    return this.weatherService.getForecastWeather(city, days)
-      .pipe(map(data => this.weatherTransformService.toCityWeatherFavorite(data)));
+      this.weatherService.getForecastWeather(city, 1)
+        .pipe(
+          map(data => this.weatherTransformService.toCityWeatherFavorite(data))
+        )
+        .subscribe((city) => {
+          this.favorites.push(city);
+        });
+    });
+    return this.favorites;
   }
 
   checkAmountOfFavorites():boolean{
-    return this.favorites.length < this.maxAmountOfFavs;
+    return this.favoriteStateService.moreCitiesCanBeAdded();
   }
 }
-
-
-
